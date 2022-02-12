11a04e170978024e05308e1d21dad5179bf89e64
midCenturyDude7 <mgriffes@gmail.com>
2022-02-12 14:54:32 -0800

    Updating with basic structure - html and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`
 
 // list todos
 // add todos
@@ -6,9 +18,33 @@ import React from 'react';
 // delete todos
 
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS)
+
+  if (loading) return <div>Loading todos, yo!...</div>
+  if (error) return <div>Error fetching todos, yo!</div>
+
   return (
-    <div>
-      app
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl1">
+      <h1 className="f2-1">GraphQL Todo Checklist<span role="img" aria-label="Checkmark"> ✅</span></h1>      
+      {/* Todo Form */}
+      <form>
+        <input 
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button type="submit">Create</button>
+      </form>
+      {/* Todo List */}
+      <div>
+        {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span>
+              {todo.text}
+            </span>
+            <button>&times;</button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
