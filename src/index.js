9c0aa3d68cc17e5d3711574c64c1c460ba5fc806
midCenturyDude7 <mgriffes@gmail.com>
2022-02-12 14:00:26 -0800

    Initializing app structure/config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient, { gql } from 'apollo-boost'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: 'https://react-todo-graphql-app.herokuapp.com/v1/graphql'
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+client.query({
+  query: gql`
+    query getTodos {
+      todos {
+        done
+        id
+        text
+      }
+    }  
+  `
+}).then(data => console.log(data));
+
+ReactDOM.render(<App />, document.getElementById('root'));
