e6f00fde1743ee0b81fe01adcf65b5cc537ced14
midCenturyDude7 <mgriffes@gmail.com>
2022-02-12 21:20:46 -0800

    Creating the basic structure/framework for frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
+// list todos
 const GET_TODOS = gql`
   query getTodos {
     todos {
@@ -12,36 +13,54 @@ const GET_TODOS = gql`
   }
 `
 
-// list todos
-// add todos
 // toggle todos
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
+
+// add todos
+
 // delete todos
 
 function App() {
-  const { data, loading, error } = useQuery(GET_TODOS)
+  const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
 
   if (loading) return <div>Loading todos, yo!...</div>
   if (error) return <div>Error fetching todos, yo!</div>
+
+  async function handleToggleTodo({ id, done}) {
+    const data = await toggleTodo({ variables: { id, done: !done } })
+    console.log(data)
+  }
 
   return (
     <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl1">
       <h1 className="f2-1">GraphQL Todo Checklist<span role="img" aria-label="Checkmark"> ✅</span></h1>      
       {/* Todo Form */}
-      <form>
-        <input 
+      <form className="mb">
+        <input className="pa2 f4 b--dashed"
           type="text"
           placeholder="Write your todo"
         />
-        <button type="submit">Create</button>
+        <button className="pa2 f4 bg-green b--dashed" type="submit">Create</button>
       </form>
       {/* Todo List */}
-      <div>
+      <div className="flex items-center justify-center flex-column">
         {data.todos.map(todo => (
-          <p key={todo.id}>
-            <span>
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && 'strike'} `}>
               {todo.text}
             </span>
-            <button>&times;</button>
+            <button className="bg-transparent bn f4"><span className="red">&times;</span></button>
           </p>
         ))}
       </div>
